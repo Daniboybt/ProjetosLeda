090fcc97ee9777ebbf910e024806c5ccdf8853fe
Danielbt <daniel.torres@ccc.ufcg.edu.br>
2018-09-03 10:23:16 -0300

    Adicionando ultimos projetos

--- R17-02-Rot-ArvoreB-environment/src/main/java/adt/btree/BTreeImpl.java
+++ R17-02-Rot-ArvoreB-environment/src/main/java/adt/btree/BTreeImpl.java
@@ -22,12 +22,19 @@ public class BTreeImpl<T extends Comparable<T>> implements BTree<T> {
 
 	@Override
 	public int height() {
-		return height(this.root);
+		if(!this.root.isEmpty()) {
+			return height(this.root);
+		}
+		return 0;
 	}
 
 	private int height(BNode<T> node) {
 		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		//throw new UnsupportedOperationException("Not Implemented yet!");
+		if(!node.isLeaf()) {
+			return 1 + height(node.getChildren().getFirst());
+		}
+		return 1;
 	}
 
 	@Override
@@ -38,14 +45,47 @@ public class BTreeImpl<T extends Comparable<T>> implements BTree<T> {
 
 	@Override
 	public int size() {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		if(!this.root.isEmpty()) {
+			return size(this.root); 
+		}
+		return 0;
+	}
+
+	private int size(BNode<T> current) {
+		int res = current.getElements().size();
+		if(!current.isLeaf()) {
+			for(int i = 0; i < current.getChildren().size(); i++) {
+				res += size(current.getChildren().get(i));
+			}	
+		}
+		return res;
 	}
 
 	@Override
 	public BNodePosition<T> search(T element) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		if(!this.root.isEmpty()) {
+			return this.search(element, this.root);
+		}
+		return null;
+	}
+
+	private BNodePosition<T> search(T element, BNode<T> current) {
+		BNodePosition<T> node = new BNodePosition<T>();
+		int positionNode;
+		for(int i = 0; i < current.getElements().size(); i++) {
+			if(element.compareTo(current.getElementAt(i)) < 0) {
+				positionNode = i;
+				break;
+			}else if(element.compareTo(current.getElementAt(i)) > 0) {
+				positionNode = i + 1;
+			}else {
+				return new BNodePosition(current, i);
+			}
+		}
+		if(!current.isLeaf()) {
+			
+		}
+		return null;
 	}
 
 	@Override
